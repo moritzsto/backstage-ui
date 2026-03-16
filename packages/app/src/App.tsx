f9b70ca0dab4f4687f5d6cf769d439ac2e25a3fd
Karthik <karthik.jk11@gmail.com>
2026-03-16 12:02:19 +0530

    prevent duplicate widget additions via prop
    Signed-off-by: Karthik <karthik.jk11@gmail.com>

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -123,7 +123,7 @@ const customHomePageModule = createFrontendModule({
                   <HeaderWorldClock clockConfigs={clockConfigs} />
                 </Header>
                 <Content>
-                  <CustomHomepageGrid>
+                  <CustomHomepageGrid preventDuplicateWidgets>
                     {widgets.map((widget, index) => (
                       <Fragment key={widget.name ?? index}>
                         {widget.component}
